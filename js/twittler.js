215b11667e979b4928bb32ede712d0afd84f7dee
Brian Boyko <brian.boyko@gmail.com>
2015-07-16 17:11:43 -0500

    Figured out how to do graceful cleanup so that the page doesn't get crazy.

--- js/twittler.js
+++ js/twittler.js
@@ -1,19 +1,32 @@
-     $(document).ready(function(){
-        var $body = $('body'); 
-        $body.html('');
+     $(document).ready(function() {
+         var $body = $('body');
+         $body.html('');
 
-          var showTweets = function (interval){
-            $('.newTweets').removeClass('newTweets');
-            while(streams.home.length > 0){
-                  var tweet = streams.home.shift();
-                  var $tweet = $('<div></div>');
-                  $tweet.html('@' + '<a class=href="#">' + tweet.user + '</a>: ' + tweet.message + '<span class="timestamp"> time: ' + tweet.created_at + '</span>');
-                  $tweet.prependTo($body);
-                  $tweet.addClass('tweet');
-                  $tweet.addClass(tweet.user);
-                  $tweet.addClass('newTweets');
-            }
-          }
-
-          setInterval(showTweets, 1500);
-      });
+         var gracefulFade = function(tweetThing){
+         }
+         var globalTweetNumber = 0;
+         var showTweets = function(interval) {
+             $('.newTweets').removeClass('newTweets');
+             $('.tweet').filter(function() {
+                 return $(this).data().tweetNumber < (globalTweetNumber - 20);
+             }).fadeOut(400);
+             while (streams.home.length > 0) {
+                 var tweet = streams.home.shift();
+                 var $tweet = $('<div></div>');
+                 $tweet.html('@' + '<a href="#">' + tweet.user +
+                     '</a>: ' + tweet.message +
+                     '<span class="timeStamp"> time: ' + tweet.created_at +
+                     '</span>');
+                 $tweet.prependTo($body);
+                 $tweet.addClass('tweet');
+                 $tweet.addClass(tweet.user);
+                 $tweet.addClass('newTweets');
+                 var timestamp = Date.now();
+                 $tweet.data('timestamp', timestamp);
+                 $tweet.data('tweetNumber', globalTweetNumber);
+                 globalTweetNumber++; 
+                 console.log($tweet.data().timestamp);
+             }
+         };
+         setInterval(showTweets, 1500);
+     });
