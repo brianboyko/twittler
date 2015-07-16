3cbe321a21d1008014cd0625613997a46008c2e9
Brian Boyko <brian.boyko@gmail.com>
2015-07-16 18:01:10 -0500

    Intermediate functionality as PoC.

--- js/twittler.js
+++ js/twittler.js
@@ -1,15 +1,14 @@
      $(document).ready(function() {
          var $body = $('body');
          $body.html('');
-
-         var gracefulFade = function(tweetThing){
-         }
          var globalTweetNumber = 0;
          var showTweets = function(interval) {
              $('.newTweets').removeClass('newTweets');
              $('.tweet').filter(function() {
-                 return $(this).data().tweetNumber < (globalTweetNumber - 20);
+                 return $(this).data().tweetNumber < (
+                     globalTweetNumber - 20);
              }).fadeOut(400);
+
              while (streams.home.length > 0) {
                  var tweet = streams.home.shift();
                  var $tweet = $('<div></div>');
@@ -17,16 +16,25 @@
                      '</a>: ' + tweet.message +
                      '<span class="timeStamp"> time: ' + tweet.created_at +
                      '</span>');
-                 $tweet.prependTo($body);
+                 $tweet.fadeIn().prependTo($body);
                  $tweet.addClass('tweet');
                  $tweet.addClass(tweet.user);
                  $tweet.addClass('newTweets');
                  var timestamp = Date.now();
+                 $tweet.data('user', tweet.user);
                  $tweet.data('timestamp', timestamp);
                  $tweet.data('tweetNumber', globalTweetNumber);
-                 globalTweetNumber++; 
-                 console.log($tweet.data().timestamp);
+                 globalTweetNumber++;
+                 console.log($tweet.data().timestamp + " " + $tweet
+                     .data().user);
              }
+
+
+             $('.tweet').on('click', function() {
+                 $('.tweet').filter(function() {
+                     return ($(this).data().user ===
+                         'mracus');
+                 }).fadeOut(400)     });
          };
          setInterval(showTweets, 1500);
      });
